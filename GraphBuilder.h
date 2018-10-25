56f71e5314ea251bc00a4173869d313f554d2552
jayswamicodes <44464178+jayswamicodes@users.noreply.github.com>
2018-10-25 02:40:39 -0700

    Add files via upload

---
+++ GraphBuilder.h
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------
+Copyright (c) 2018 Author: Jagadeesh Vasudevamurthy
+file: GraphBuilder.h
+-----------------------------------------------------------------*/
+
+/*----------------------------------------------------------------
+This file has GraphBuilder class declaration
+-----------------------------------------------------------------*/
+
+/*----------------------------------------------------------------
+All includes here
+-----------------------------------------------------------------*/
+#ifndef GraphBuilder_H
+#define GraphBuilder_H
+
+#include "Graph.h"
+
+/*----------------------------------------------------------------
+Declaration of GraphBuilder class
+WRITE CODE
+-----------------------------------------------------------------*/
+class GraphBuilder {
+public:
+	GraphBuilder(Graph& g, const vector<string>& s) {}
+	~GraphBuilder() {}
+};
+#endif
+
+
+//EOF
